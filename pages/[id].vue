0e007469ba51f430c9305009a0646237b0be3698
Göktuğ Ceyhan <goktugceyhan@gmail.com>
2024-07-28 03:29:15 +0300

    refactor: Improve error handling and URL validation in [id].vue

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,35 +1,54 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <p>Redirecting...</p>
+  <div class="flex justify-center items-center h-screen w-screen text-xl">
+    <p v-if="error">{{ error }}</p>
+    <p v-else>Redirecting...</p>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { type Database } from "~/types/supabase";
-import useExternalRedirect from '~/composables/useExternalRedirect'
+import useExternalRedirect from "~/composables/useExternalRedirect";
 
 const client = useSupabaseClient<Database>();
 const route = useRoute();
+const error = ref<string | null>(null);
 
 if (!route.params.id) {
-  throw createError("Not Found");
+  throw showError({
+    status: 404,
+    statusText: "Invalid URL",
+    statusMessage: "The URL is invalid.",
+  });
 }
 
 const { data } = await useAsyncData("links", async () => {
-  const { data, error } = await client
+  const { data } = await client
     .from("links")
     .select("*")
     .eq("key", route.params.id)
     .single();
 
-  if (error) {
-    throw createError("Not Found");
-  }
-
   return data;
 });
 
+function isValidUrl(url: string): boolean {
+  try {
+    new URL(url);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
 if (data.value?.url) {
-  useExternalRedirect(data.value.url, data.value.id, 302);
+  if (isValidUrl(data.value.url)) {
+    useExternalRedirect(data.value.url, data.value.id, 302);
+  } else {
+    throw showError({
+      status: 404,
+      statusText: "Invalid URL",
+      statusMessage: "The URL is invalid.",
+    });
+  }
 }
 </script>
